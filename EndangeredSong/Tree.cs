eaccbec8dbab0b9f4b2667597c6361f674717c0f
AmberJMLee <alphagirl93@yahoo.com>
2015-06-08 10:18:43 -0700

    Tree

--- EndangeredSong/Tree.cs
+++ EndangeredSong/Tree.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace EndangeredSong
 {
-    class Class1
+    class Tree : HidingPlace
     {
+        public Tree(int x, int y, int width, int height) : base(x, y, width, height)
+        {
+            
+        }
+
+        public Tree(int x, int y, int width, int height, int capacity) : base(x, y, width, height, capacity)
+        {
+        }
+
+        new public void LoadContent(ContentManager content)
+        {
+            image = content.Load<Texture2D>("EmptyRock.png");
+            filledHidingPlace = content.Load<Texture2D>("FilledRock.png");
+        }
     }
 }
