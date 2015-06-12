3158c735cdaa596d30319c24897073b64eb83a5e
Catina <carol.dahmen@gmail.com>
2015-06-12 12:32:59 -0400

    Camera Edges are fixed! YAY :0
    :+1: :+1:

--- EndangeredSong/Camera.cs
+++ EndangeredSong/Camera.cs
@@ -12,15 +12,56 @@ namespace EndangeredSong
         public Matrix transform;
         Viewport view;
         public Vector2 center;
+        int mapX;
+        int mapY;
         public Camera(Viewport newView)
         {
             view = newView;
+            mapX = 4000;
+            mapY = 3000;
         }
+
         public void Update(GameTime gameTime, Player player, int screenWidth, int screenHeight)
         {
-            center = new Vector2(player.getPosition().X + (player.getDimension().X / 2) - (screenWidth / 2), player.getPosition().Y + (player.getDimension().Y / 2) - (screenHeight / 2));
+
+            center = new Vector2();
+
+            if (player.getPosition().X + player.getDimension().X / 2 >= screenWidth / 2 && player.getPosition().X + player.getDimension().X / 2 <= mapX - screenWidth / 2)
+            {
+                center.X = player.getPosition().X + (player.getDimension().X / 2) - (screenWidth / 2);
+            }
+
+            else if (player.getPosition().X + player.getDimension().X / 2 < screenWidth/2)
+            {
+                center.X = 0;
+            }
+
+            else 
+            {
+                center.X = mapX - screenWidth;
+            }
+
+
+            if (player.getPosition().Y + player.getDimension().Y / 2 >= screenHeight / 2 && player.getPosition().Y <= mapY - screenHeight / 2)
+            {
+                center.Y = player.getPosition().Y + (player.getDimension().Y / 2) - (screenHeight / 2);
+            }
+
+            else if (player.getPosition().Y + player.getDimension().Y / 2 < screenHeight / 2)
+            {
+                center.Y = 0;
+            }
+
+            else
+            {
+                center.Y = mapY - screenHeight;
+            }
+//            center.Y = player.getPosition().Y + (player.getDimension().Y / 2) - (screenHeight / 2);
+//            center = new Vector2(player.getPosition().X + (player.getDimension().X / 2) - (screenWidth / 2), player.getPosition().Y + (player.getDimension().Y / 2) - (screenHeight / 2));
             transform = Matrix.CreateScale(new Vector3(1, 1, 0)) * Matrix.CreateTranslation(new Vector3(-center.X, -center.Y, 0));
         }
+
+
 
         public void Update(GameTime gameTime, ScreenManager menu, int screenWidth, int screenHeight)
         {
